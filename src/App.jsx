3470161fc2094bf799ec7b1b6e0b50420462eb15
Frostyflames12 <rizqyriadi02@gmail.com>
2023-12-06 21:30:48 +0700

    AddProject works
    add project works and display title to Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectsState, setProjectsSTate] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsSTate((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsSTate((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
-    
+      <Sidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,3 @@
+export default function Model({ children }) {
+  return <dialog>{children}</dialog>;
+}
